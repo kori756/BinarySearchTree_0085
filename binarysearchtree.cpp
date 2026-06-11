936e136c8fe768e6193b1f7b3fb0a9a398c04d40
kori756 <khorirachmatr@gmail.com>
2026-06-11 18:12:00 +0700

    menambahkan witch and case di program utama untuk pemilihan menu

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -138,4 +138,50 @@ int main()
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
 
-        
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word:  ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
+
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
+
+        case '5':
+            return 0;
+
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+
+    return 0;
+}
