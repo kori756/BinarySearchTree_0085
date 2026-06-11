456bd18d8234dae37178efc3748af103311b09ef
kori756 <khorirachmatr@gmail.com>
2026-06-11 18:10:02 +0700

    menambahkan logika ifuntuk postorder

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -107,4 +107,21 @@ public:
     }
 
     void postorder(Node *ptr)
-    
+    {
+        // Performs the postorder traversal of the tree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << "  ";
+        }
+    }
+};
+
